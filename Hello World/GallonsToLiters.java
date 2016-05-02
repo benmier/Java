5428757e28ff2d60013ad19ab9cf10dbe2a88876
benmier <bencmier@gmail.com>
2016-05-02 16:56:33 -0700

    Java demo

---
+++ Hello World/GallonsToLiters.java
@@ -0,0 +1,8 @@
+class GallonsToLiters {
+    public static void main(String args[]) {
+    	double gallons = 10;
+    	double liters = gallons*3.78541;
+    	System.out.println("Gallons: "+gallons);
+    	System.out.println("Liters: "+liters);
+    }
+}
