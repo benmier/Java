b87dd8005a195b6381252b3b7df378d528e1ea47
benmier <bencmier@gmail.com>
2016-05-02 17:06:43 -0700

    Java demo

---
+++ Hello World/GallonsToLitersTable.java
@@ -0,0 +1,9 @@
+class GallonsToLitersTable {
+    public static void main(String args[]) {
+    	for(int gallons=1; gallons<=100; gallons++){
+    		System.out.println(gallons+" gallons is "+gallons*3.78541+" liters");
+    		if(gallons%10==0)
+    			System.out.println("\n");
+    	}
+    }
+}
