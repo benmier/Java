2c46e80b96701454a3bba1c2f146fdda42c1360f
benmier <bencmier@gmail.com>
2016-05-03 11:36:56 -0700

    Java demo

--- Chapter 3/VehicleDemo.java
+++ Chapter 3/VehicleDemo.java
@@ -2,19 +2,19 @@ class Vehicle {
     int passengers; // number of passengers
     int fuelcap; // fuel capacity in gallons
     int mpg; // fuel consumption in miles per gallon
+    int range;
 }
 // This class declares an object of type Vehicle.
 class VehicleDemo {
     public static void main(String args[]) {
         Vehicle minivan = new Vehicle();
-        int range;
         // assign values to fields in minivan
         minivan.passengers = 7;
         minivan.fuelcap = 16;
         minivan.mpg = 21;
         // compute the range assuming a full tank of gas
-        range = minivan.fuelcap * minivan.mpg;
+        minivan.range = minivan.fuelcap * minivan.mpg;
         System.out.println("Minivan can carry " + minivan.passengers +
-            " with a range of " + range);
+            " with a range of " + minivan.range);
     }
 }
