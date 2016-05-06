6826e2c8439dc6d248298eff5f253bcdd2631e5d
benmier <bencmier@gmail.com>
2016-05-05 17:30:59 -0700

    Java demo

--- Chapter 6/QSDemo.java
+++ Chapter 6/QSDemo.java
@@ -1,12 +1,12 @@
 class Quicksort {
 // Set up a call to the actual Quicksort method.
-	static void qsort(char items[]) {
+	static void qsort(int items[]) {
 		qs(items, 0, items.length - 1);
 	}
 // A recursive version of Quicksort for characters.
-	private static void qs(char items[], int left, int right) {
+	private static void qs(int items[], int left, int right) {
 		int i, j;
-		char x, y;
+		int x, y;
 		i = left; j = right;
 		x = items[(left + right) / 2];
 		do {
@@ -25,7 +25,7 @@ class Quicksort {
 }
 class QSDemo {
 	public static void main(String args[]) {
-		char a[] = { 'd', 'x', 'a', 'r', 'p', 'j', 'i' };
+		int a[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
 		int i;
 		System.out.print("Original array: ");
 		for (i = 0; i < a.length; i++)
@@ -34,6 +34,7 @@ class QSDemo {
 // now, sort the array
 		Quicksort.qsort(a);
 		System.out.print("Sorted array: ");
-		System.out.print(a);
+		for (i = 0; i < a.length; i++)
+			System.out.print(a[i]);
 	}
 }
